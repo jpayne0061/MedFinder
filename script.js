46d3014483ef692371319c60303ed22d394008ba
jpayne0061 <skatejames0061@gmail.com>
2017-03-19 17:44:27 +0000

    using module pattern now

--- script.js
+++ script.js
@@ -1,61 +1,104 @@
-
-$("#search-form").keyup(function(){
-    var searchWord = $(this).val();
-    console.log(searchWord);
-    if(searchWord.length == 1){
+var GetDrugs = function () {
+    var getDrugInfo = function(fileName){
+        $.ajax({url: "drug_soup/" + fileName + ".html", success: function(result){
+            $("#messages").prepend(result);
+            
+        }});
+        
+    };    
+    
+    var getDrugNames = function(searchWord){
         $.ajax({url: "drug_names/" + searchWord + ".html", success: function(result){
             
             $("#tester").empty();
             $("#tester").append("<span class='btn btn-danger'>Clear All</span>");
             $("#tester").append("<span class='btn btn-warning'>Clear Med Names</span>");
             $("#tester").append(result);
-         }});
-    }
-    
-    if(searchWord.length > 1){
-        $("#tester .btn-success").each(function(){
-            $(this).hide();
-          if(this.innerHTML.search(searchWord.toUpperCase()) == 0){
-              $(this).show();
-          }
-        });
-    }  
-    
-    if(searchWord.length == 0){
-        $("#tester").empty();
-    }
-
-});
-
-
-
-$(".btn").click(function(){
-    fileName = this.innerHTML;
-    $.ajax({url: "drug_names/" + fileName + ".html", success: function(result){
-        $("#tester").empty();
-        $("#tester").append("<span class='btn btn-danger'>Clear List</span>");
-        $("#tester").append("<span class='btn btn-warning'>Clear Med Names</span>");
-        $("#tester").append(result);
-    }});
-});
-
-$(document.body).on("click", ".btn-danger", function(){
-    $("#tester").empty();
-    $("#messages").empty();
-});
-
-$(document.body).on("click", ".btn-warning", function(){
-    $("#tester").empty();
-});
-
-$(document.body).on("click", "span", function(){
-    fileName = this.innerHTML;
-    $(this).css("background-color", "#95d095");
-    $.ajax({url: "drug_soup/" + fileName + ".html", success: function(result){
-        $("#messages").prepend(result);
+        }});
+    };
         
-    }});
-});
+    return {
+        getDrugInfo : getDrugInfo,
+        getDrugNames : getDrugNames
+        
+    };
+}();
+
+
+
+
+var Events = function(getDrugs){
+    var clickLetter = function(){
+        $(".btn").click(function(){
+            fileName = this.innerHTML;
+            getDrugs.getDrugNames(fileName);
+        });
+    };
+    
+    var clickClearAll = function(){
+        $(document.body).on("click", ".btn-danger", function(){
+            $("#tester").empty();
+            $("#messages").empty();
+        });
+        
+    };
+    
+    var clickClearNames = function(){
+        $(document.body).on("click", ".btn-warning", function(){
+            $("#tester").empty();
+        });
+    };
+    
+    var clickDrugName = function(){
+        $(document.body).on("click", "span", function(){
+            fileName = this.innerHTML;
+            $(this).css("background-color", "#95d095");
+            getDrugs.getDrugInfo(fileName);
+        });
+        
+    };
+    
+    var searchFormKeyEvent = function(){
+        
+        $("#search-form").keyup(function(){
+            var searchWord = $(this).val();
+            if(searchWord.length == 1){
+                getDrugs.getDrugNames(searchWord);
+            }
+            
+            if(searchWord.length > 1){
+                $("#tester .btn-success").each(function(){
+                    $(this).hide();
+                    if(this.innerHTML.search(searchWord.toUpperCase()) == 0){
+                        $(this).show();
+                    }
+                });
+            }  
+            
+            if(searchWord.length == 0){
+                $("#tester").empty();
+            }
+        
+        });
+    };
+    
+    var init = function(){
+        clickLetter();
+        clickClearAll();
+        clickClearNames();
+        clickDrugName();
+        searchFormKeyEvent();
+    };
+    
+    return {
+        init : init
+    };
+    
+}(GetDrugs);
+
+Events.init();
+
+
 
 
 
